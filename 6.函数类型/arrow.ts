4d398cb2d9e9058a52b6331bdbae391312f3809c
fxzer <fxzer8888@163.com>
2024-07-16 23:24:47 +0800

    feat: 添加笔记

---
+++ 6.函数类型/arrow.ts
@@ -0,0 +1,33 @@
+/** 箭头函数类型注解 */
+export const foo1 = <T>(x: T): T => x
+export const foo2 = <T extends {}>(x: T): T => x
+export const foo3 = <T extends Record<string, unknown>>(x: T): T => x
+export const foo4: <T>(x: T) => T = x => x
+
+// foo3(1) // Error ❌
+foo3({ a: 1, b: 2 })
+foo4(1)
+
+function identity<T>(arg: T): T {
+  console.log(arg)
+  return arg
+}
+
+type CurrentAuthorityType = 'admin' | 'user'
+
+function renderAuthorize<T>(Authorized: T): ((currentAuthority: CurrentAuthorityType) => T) {
+  return (
+    currentAuthority: CurrentAuthorityType,
+  ): T => {
+    return 'admin' as unknown as T
+  }
+}
+type CreateArrayFunc = <T>(length: number, value: T) => Array<T>
+let createArray: CreateArrayFunc
+createArray = function<T>(length: number, value: T): Array<T> {
+  let result: T[] = []
+  for (let i = 0; i < length; i++) {
+    result[i] = value
+  }
+  return result
+}
