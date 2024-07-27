fd34c5f45d3c611e8ce67c415b13547c4c099c26
fxzer <fxzer8888@163.com>
2024-07-27 13:48:11 +0800

    feat: 添加笔记

--- 高级技巧/变量取属性.ts
+++ 高级技巧/变量取属性.ts
@@ -21,7 +21,8 @@ type Concat = 'h'['concat'] // String.prototype.concat 的类型定义
 type N = 1
 type ToFixed = N['toFixed'] // Number.prototype.toFixed 的类型定义
 
-/* 获取类型所有属性 key */
+/* ====   对象 ===== */
+/* 获取对象类型所有属性 key */
 type Keys = 'a' | 'b' | 'c'
 type Obj = {
   [p in Keys]: string
@@ -38,6 +39,26 @@ type ObjKeys1 = keyof Obj1 // 为什么不是 'a' | 'b' | 'c'  ？？
 
 /* 获取对象所有属性值 */
 type ObjValues = Obj[ObjKeys1] //  string ??
+
+/* 获取对象所有属性的类型 */
+export const routes = {
+  home: '/',
+  about: '/about',
+  contact: '/contact',
+}
+
+export type Routes = typeof routes
+export type RoutesKeys = keyof Routes // 'home' | 'about' | 'contact'
+export type RoutesValues = Routes[RoutesKeys] // string  <== 拿不到具体的联合类型
+
+export const routes1 = {
+  home: '/',
+  about: '/about',
+  contact: '/contact',
+} as const
+export type Routes1 = typeof routes1
+export type RoutesKeys1 = keyof Routes1 // 'home' | 'about' | 'contact'
+export type RoutesValues1 = Routes1[RoutesKeys1] // "/" | "/about" | "/contact"
 
 /* ⚠️箭头函数类型和空对象没有 key。例如: */
 type F = () => void
